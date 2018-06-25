10e2d9bd78a3c390a112e818008b05f52e96432b
leonardo.nascimento <leomn138@gmail.com>
2018-06-24 22:13:50 -0300

    transformation service initial commit

---
+++ transformation-service/src/main/java/com/datasetstolinkeddata/transformation/TransformationApplication.java
@@ -0,0 +1,12 @@
+package com.datasetstolinkeddata.transformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformationApplication.class, args);
+	}
+}
